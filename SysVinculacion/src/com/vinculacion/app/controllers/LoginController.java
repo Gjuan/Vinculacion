7a3338041a6819a4b2edee68369de3703de88804
Gjuan <juangabrielestrada1994@gmail.com>
2018-01-26 20:10:46 -0500

    Se agrego vista de ejemplo de login y un controlador de login

---
+++ SysVinculacion/src/com/vinculacion/app/controllers/LoginController.java
@@ -0,0 +1,18 @@
+package com.vinculacion.app.controllers;
+
+import com.vinculacion.app.factory.FactorFactory;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginController extends FactorFactory implements ActionListener{
+
+    public LoginController() {
+    }
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+
+    }
+        
+}
